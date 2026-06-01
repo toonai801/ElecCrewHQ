7bb46121ad7b8e05778be0c303a8aa22751c1989
TAHNTOON <sandrewbooth2@gmail.com>
2026-05-31 18:26:50 -0600

    Allow Supabase uploaded images

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "mloilaipbnosqifyhncj.supabase.co",
+      },
     ],
   },
 };
